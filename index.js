7b4bc37c8ecbf5d90a333f43df451975c41b9a8b
Abel <abel15655@gmail.com>
2021-07-13 11:59:36 +0100

    importing files

---
+++ index.js
@@ -0,0 +1,52 @@
+let navLinks = document.querySelectorAll("header .topbar nav ul li a")
+
+
+for(let i=0; i<navLinks.length; i++) {
+    navLinks[i].onclick = () => {
+        for(let x=0; x<navLinks.length; x++) {
+            navLinks[x].classList.remove("active");
+            navLinks[i].classList.add("active");
+            menu.classList.toggle("active")
+    }
+}
+}
+
+let menuButton = document.querySelector(".menu-button");
+let menu = document.querySelector("header .topbar nav");
+
+menuButton.addEventListener("click", () => {
+    menu.classList.toggle("active");
+})
+
+let form = document.getElementById("contact-form");
+
+let username = form.elements["name"];
+let email = form.elements["email"];
+
+let msg = document.querySelector(".msg");
+
+
+form.addEventListener("submit", function(e) {
+    
+    msgArr = [];
+    msg.textContent = "";
+
+    if(username.value === "") {
+        msgArr.push("The name field is required")
+    }
+    if(username.value.length < 4) {
+        msgArr.push("Name must be more than 3 characters")
+    }
+    if(email.value === "") {
+        msgArr.push("The email field is required")
+    }
+
+    if(msgArr.length !== 0) {
+        e.preventDefault()
+        msgArr.forEach(messages => {
+            let p = document.createElement("p");
+            p.textContent = messages;
+            msg.appendChild(p)
+        })
+    }
+})
