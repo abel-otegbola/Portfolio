ab4f6e490be8ae736ae99881cc223d19455d94c4
abel-otegbola <abel15655@gmail.com>
2022-02-12 23:53:33 +0100

    fixing /msg error

--- components/config/urls.js
+++ components/config/urls.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://AbelOtegbola.vercel.app'
+export const server = 'http://localhost:3000'
