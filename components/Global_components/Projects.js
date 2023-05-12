5f79a20f2c400c60900a7b6bc029b8ab0f378711
abel-otegbola <abel15655@gmail.com>
2023-05-13 00:48:59 +0100

    feat: add additional projects to portfolio

--- components/Global_components/Projects.js
+++ components/Global_components/Projects.js
@@ -5,8 +5,13 @@ import { FaGithub, FaLink } from "react-icons/fa"
 
 const Projects = () => {
     const contracts = [
-        { id: 1, title: "SOLTOOLS", img: "/projects/Cleanlist.png", summary: "A landing page for soltools which is a one-stop shop providing Defi support on the solana blockchain", Inspiration: "The inspiration behind the design and tech stack used is to provide a simple, informative and easy to understand landing page. The colors carefully used to match the brand logo, headings and call to actions to give the visitors an understanding of the services offered", how: "The landing page was built with HTML, CSS, JS and Bootstrap to keep it light and simple", tech: ["HTML", "CSS", "JAVASCRIPT", "BOOTSTRAP"] },
-        { id: 2, title: "NEPAL BEST DEALS", img: "/projects/nepalbestdeal.png", summary: "A landing page for soltools which is a one-stop shop providing Defi support on the solana blockchain", Inspiration: "The inspiration behind the design and tech stack used is to provide a simple, informative and easy to understand landing page. The colors carefully used to match the brand logo, headings and call to actions to give the visitors an understanding of the services offered", how: "The landing page was built with HTML, CSS, JS and Bootstrap to keep it light and simple", tech: ["HTML", "CSS", "JAVASCRIPT", "BOOTSTRAP"] },
+        { id: 1, title: "SOLTOOLS", img: "/projects/soltools.png", summary: "A landing page for soltools which is a one-stop shop providing Defi support on the solana blockchain", Inspiration: "The inspiration behind the design and tech stack used is to provide a simple, informative and easy to understand landing page. The colors carefully used to match the brand logo, headings and call to actions to give the visitors an understanding of the services offered", how: "The landing page was built with HTML, CSS, JS and Bootstrap to keep it light and simple", tech: ["HTML", "CSS", "JAVASCRIPT", "BOOTSTRAP"] },
+        { id: 2, title: "NEPAL BEST DEALS", img: "/projects/nepalbestdeal.png", summary: "An ecommerce website offering users nepal products such as clothes and apparels, toys, herbs and lots more", Inspiration: "The project was to redesign the entire website and make it more visually appealing. Using a simple theme and builder I optimized the user interface and experience", how: "The website was built with wordpress. I used an awesome wordpress theme together with elementor builder.", tech: ["WORDPRESS", "ELEMENTOR", "WOOCOMMERCE"] },
+    ]
+
+    const side_projects = [
+        { id: 1, title: "FORMILIO", img: "/projects/formilio.png", summary: "A website to easily generate endpoints for your static forms and start collecting data in no time.", Inspiration: "Creating static websites are easy, but handling informations from users has a lot of work. Some of which are making sure informaton is genuine, secure and stored properly. Formilio will serve as your data storage to store all your form submissions.", how: "The website was built with NEXT.JS, MONGODB, and other libraries and tools", tech: ["NEXT.JS", "REACT", "MONGODB", "CHART.JS"] },
+        { id: 2, title: "CLOCKS APP", img: "/projects/clock-app.png", summary: "A fun clock ui website displaying different clock designs such as neomorphism, glassmorphism, apple wristwatch, table clock and lots more", Inspiration: "A simple frontend application, with easy to read codebase, opened to contributors and unique in design and functionality.", how: "Easy to understand and replicate Javascript, css and html codes with minimal design.", tech: ["HTML", "CSS", "JAVASRCIPT"] },
     ]
     
     const opensource = [
@@ -21,14 +26,35 @@ const Projects = () => {
             {
                 contracts.map(project => (
                     <div key={project.id} className="my-10" data-aos="fade-up">
-                        <Link href={`/project/${project.title.toLowerCase()}`}><a><Image src={project.img} alt={project.title} className="rounded-lg" width={700} height={300} /></a></Link>
+                        <Link href={`/project/${project.title.toLowerCase()}`}><a><Image src={project.img} alt={project.title} className="rounded-lg" width={700} height={320} /></a></Link>
                         <div className="p-2">
                             <h1 className="text-xl font-semibold py-4"><Link href={`/project/${project.title.toLowerCase()}`}><a>{project.title}</a></Link></h1>
                             <p className="leading-[30px]">{project.summary}...<Link href={`/project/${project.title.toLowerCase()}`} ><a className="text-fuchsia-300 hover:text-fuchsia-600">View more</a></Link></p>
                             <div className="flex flex-wrap gap-4 py-4 text-[10px]">
                                 {
                                     project.tech.map((skill, i) => (
-                                        <span key={i} className="p-2 px-6 rounded-[30px] bg-fuchsia-500/[0.3]">{skill}</span>
+                                        <span key={i} className="p-[2px] px-4 border border-fuchsia-300/[0.4] rounded-[30px] bg-fuchsia-500/[0.3]">{skill}</span>
+                                ))
+                                }
+                            </div>
+                        </div>
+                    </div>
+                ))
+            }
+
+            
+            <h3 className="font-semibold pb-4 underline underline-offset-8 decoration-1 decoration-gray-700 w-full" id="side-projects">Side Projects</h3>
+            {
+                side_projects.map(project => (
+                    <div key={project.id} className="my-10" data-aos="fade-up">
+                        <Link href={`/project/${project.title.toLowerCase()}`}><a><Image src={project.img} alt={project.title} className="rounded-lg" width={700} height={320} /></a></Link>
+                        <div className="p-2">
+                            <h1 className="py-4"><Link href={`/project/${project.title.toLowerCase()}`}><a className="text-xl font-semibold">{project.title}</a></Link></h1>
+                            <p className="leading-[30px]">{project.summary}...<Link href={`/project/${project.title.toLowerCase()}`} ><a className="text-fuchsia-300 hover:text-fuchsia-600">View more</a></Link></p>
+                            <div className="flex flex-wrap gap-4 py-4 text-[10px]">
+                                {
+                                    project.tech.map((skill, i) => (
+                                        <span key={i} className="p-[2px] px-4 border border-fuchsia-300/[0.4] rounded-[30px] bg-fuchsia-500/[0.3]">{skill}</span>
                                 ))
                                 }
                             </div>
