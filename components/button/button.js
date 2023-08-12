d5146134c5000fb40bd782ec60c7cb2855b0d3cb
abel-otegbola <abel15655@gmail.com>
2023-08-12 14:04:38 +0100

    style: change border and color for darkmode secondary

--- components/button/button.js
+++ components/button/button.js
@@ -2,8 +2,8 @@ const Button = ({ text, to, type }) => {
     return (
         <a 
             href={to} 
-            className={`block w-fit flex items-center text-center sm:py-[12px] py-[10px] px-6 hover:bg-primary rounded
-                ${type === "primary" ? "bg-primary text-white" : "border border-primary text-primary hover:text-white"}`}
+            className={`block w-fit flex items-center text-center sm:py-[12px] py-[10px] px-6 hover:bg-primary/[0.7] rounded
+                ${type === "primary" ? "bg-primary text-white" : "border border-primary dark:border-gray-100/[0.5] dark:text-gray-100/[0.5] text-primary hover:text-white"}`}
         >
             {text}
         </a>
