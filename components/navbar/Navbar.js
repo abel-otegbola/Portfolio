75a013ce738bfd13d86bf12a3c313b321ee83b24
abel-otegbola <abel15655@gmail.com>
2023-08-12 14:06:35 +0100

    feat: add theme functionality and close nav on link click

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -1,10 +1,20 @@
 import Link from "next/link";
-import { useState } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
+import { useEffect, useState } from "react";
+import { FaBars, FaMoon, FaSun, FaTimes } from "react-icons/fa";
 import Button from "../button/button";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
+  const [theme, setTheme] = useState("")
+
+  useEffect(() => {
+    setTheme(localStorage.getItem("theme"))
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark')
+    } else {
+    document.documentElement.classList.remove('dark')
+    }
+  }, [theme])
 
   return (
     <div className="flex sticky top-0 shadow-sm bg-white dark:bg-body items-center justify-between md:p-3 p-5 md:px-[7.5%] px-[5%] z-10">
@@ -13,13 +23,13 @@ const Navbar = () => {
       </h1>
       <nav
         className={`lg:static fixed md:flex items-center gap-8 right-0 top-0 bg-white dark:bg-body overflow-hidden transition-all duration-700 z-20 lg:h-[auto] h-screen ${
-          open ? "lg:w-[auto] w-[100%] md:p-0 p-[5%]" : "lg:w-[auto] w-0"
+          open ? "lg:w-[auto] w-[75%] md:p-0 p-[5%]" : "lg:w-[auto] w-0"
         }`}
       >
-        <Link href={"#portfolio"} className="md:inline block md:p-0 px-[30px] py-5">Portfolio</Link>
-        <Link href={"#experience"} className="md:inline block md:p-0 px-[30px] py-5">Experience</Link>
-        <Link href={"#experience"} className="md:inline block md:p-0 px-[30px] py-5">Skills</Link>
-        <div className="md:p-0 px-[30px] py-5">
+        <Link href={"/"} className="md:inline block md:p-0 px-[30px] py-5" onClick={() => setOpen(false)}>Home</Link>
+        <Link href={"#about"} className="md:inline block md:p-0 px-[30px] py-5" onClick={() => setOpen(false)}>About</Link>
+        <Link href={"#portfolio"} className="md:inline block md:p-0 px-[30px] py-5" onClick={() => setOpen(false)}>Portfolio</Link>
+        <div className="md:p-0 px-[30px] py-5" onClick={() => setOpen(false)}>
           <Button text={"Hire me"} to={"#contacts"} type={"primary"} />
         </div>
       </nav>
