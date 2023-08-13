2ee49ef20192c90febc0ba35cb4bcdb6547dc71d
abel-otegbola <abel15655@gmail.com>
2023-08-13 22:55:48 +0100

    fix: twitter card meta not working

--- components/Layout.js
+++ components/Layout.js
@@ -23,8 +23,9 @@ const Layout = ({ children }) => {
                 <meta property="og:image:secure_url" content="/abelo.PNG" />
                 <meta name="twitter:title" content="Abel Otegbola" />
                 <meta name="twitter:description" content="Full stack website developer and UI designer with experience building amazing products." />
-                <meta name="twitter:image:src" content="https://abelo.tech/img.png" />
-                <meta name="twitter:card" content="/abelo.PNG"/>
+                <meta name="twitter:image" content="https://abelo.tech/img.png" />
+                <meta name="twitter:card" content="summary"/>
+                <meta name="twitter:url" content="https://abelo.tech"/>
 
                 <link rel="manifest" href="/manifest.json" />
             </Head>
