5457b647e37c1ee3487af4fa8dcebc6c2c03017e
abel-otegbola <abel15655@gmail.com>
2024-03-21 05:54:11 +0100

    fix: error in header due to react-icon

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import { PiFile, PiGithubLogo, PiTwitterLogo, PiLinkedinLogo } from "react-icons/pi";
+import { FaFile, FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 
 const Header = () => {
@@ -17,10 +17,10 @@ const Header = () => {
       <div className="flex gap-2">
         <Image src={"/img.webp"} width={200} height={150} alt="Abel Otegbola" className="rounded mb-4" data-aos="zoom-out"></Image>
         <div className="flex flex-col gap-4">
-          <a className="p-2 text-[20px] border border-gray-500/[0.2] w-fit rounded" href={"https://drive.google.com/file/d/1FK09zavZtnsVuExYwwJ45wsTr-6N6_Cd/view?usp=sharing"}><PiFile /></a>
-          <a className="p-2 text-[20px] border border-gray-500/[0.2] w-fit rounded" href={"https://github.com/abel-otegbola"}><PiGithubLogo /></a>
-          <a className="p-2 text-[20px] border border-gray-500/[0.2] w-fit rounded" href={"https://twitter.com/Abel_Otegbola"}><PiTwitterLogo /></a>
-          <a className="p-2 text-[20px] border border-gray-500/[0.2] w-fit rounded" href={"https://linkedin.com/in/abel-otegbola"}><PiLinkedinLogo /></a>
+          <a className="p-2 text-[20px] border border-gray-500/[0.2] w-fit rounded" href={"https://drive.google.com/file/d/1FK09zavZtnsVuExYwwJ45wsTr-6N6_Cd/view?usp=sharing"}><FaFile /></a>
+          <a className="p-2 text-[20px] border border-gray-500/[0.2] w-fit rounded" href={"https://github.com/abel-otegbola"}><FaGithub /></a>
+          <a className="p-2 text-[20px] border border-gray-500/[0.2] w-fit rounded" href={"https://twitter.com/Abel_Otegbola"}><FaTwitter /></a>
+          <a className="p-2 text-[20px] border border-gray-500/[0.2] w-fit rounded" href={"https://linkedin.com/in/abel-otegbola"}><FaLinkedin/></a>
         </div>
       </div>
     </header>
