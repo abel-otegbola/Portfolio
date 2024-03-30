cb6eeca7ae5b165afe0a34e02525868818be1eb1
abel-otegbola <abel15655@gmail.com>
2024-03-30 12:45:17 +0100

    style: remove opacity in hover bg color for button

--- components/button/button.js
+++ components/button/button.js
@@ -3,7 +3,7 @@ const Button = ({ text, to, type }) => {
         <a 
             href={to} 
             aria-label={"Link to " + to}
-            className={`block w-fit flex items-center text-center py-[10px] px-6 hover:bg-primary/[0.2] rounded
+            className={`block w-fit flex items-center text-center py-[10px] px-6 hover:bg-primary hover:text-white rounded
                 ${type === "primary" ? "bg-primary text-white" : "border border-gray-500/[0.2]"}`}
         >
             {text}
