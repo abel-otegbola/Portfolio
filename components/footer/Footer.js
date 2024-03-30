5e4b52c764e399f2a533da99da6b91675b403676
abel-otegbola <abel15655@gmail.com>
2024-03-30 12:46:18 +0100

    style: add bg color to footer

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -3,7 +3,7 @@ import { FaHeart } from 'react-icons/fa'
 
 const Footer = () => {
     return (
-        <footer data-testid="footer" className="flex items-center justify-center gap-2 p-[10%] text-center">
+        <footer data-testid="footer" className="flex items-center dark:bg-[#000]/[0.1] justify-center gap-2 p-10 text-center">
             Built with <FaHeart className='text-red-600 text-lg' /> | Copyright &copy; {new Date().getFullYear()} 
         </footer>
     )
