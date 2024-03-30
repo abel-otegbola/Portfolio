32ecd8c862092abc56edb05f4536422ce03b8634
abel-otegbola <abel15655@gmail.com>
2024-03-30 12:46:53 +0100

    fix: color, texts and icons change in header

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import { FaFile, FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { PiFile, PiGithubLogo, PiTwitterLogo, PiLinkedinLogo } from "react-icons/pi";
 
 
 const Header = () => {
@@ -8,7 +8,7 @@ const Header = () => {
       <div className="relative md:w-[50%]">
 
         <h1 className="md:w-[70%] -leading-[10px] sm:text-3xl text-3xl dark:text-slate-200" data-aos="fade-down">
-          A <span className="underline">developer</span> cutting through the space of building amazing website projects. 
+          A <span className="underline">developer</span> passionate about building amazing website projects. 
         </h1>
         
         <p className="py-5" data-aos="fade-down" data-aos-delay="400">Full Stack Website Developer</p>
@@ -17,10 +17,10 @@ const Header = () => {
       <div className="flex gap-2">
         <Image src={"/img.webp"} width={200} height={150} alt="Abel Otegbola" className="rounded mb-4" data-aos="zoom-out"></Image>
         <div className="flex flex-col gap-4">
-          <a className="p-2 text-[20px] border border-gray-500/[0.2] w-fit rounded" href={"https://drive.google.com/file/d/1FK09zavZtnsVuExYwwJ45wsTr-6N6_Cd/view?usp=sharing"}><FaFile /></a>
-          <a className="p-2 text-[20px] border border-gray-500/[0.2] w-fit rounded" href={"https://github.com/abel-otegbola"}><FaGithub /></a>
-          <a className="p-2 text-[20px] border border-gray-500/[0.2] w-fit rounded" href={"https://twitter.com/Abel_Otegbola"}><FaTwitter /></a>
-          <a className="p-2 text-[20px] border border-gray-500/[0.2] w-fit rounded" href={"https://linkedin.com/in/abel-otegbola"}><FaLinkedin/></a>
+          <a className="p-2 text-[20px] border border-gray-500/[0.2] w-fit rounded" href={"https://drive.google.com/file/d/1FK09zavZtnsVuExYwwJ45wsTr-6N6_Cd/view?usp=sharing"}><PiFile /></a>
+          <a className="p-2 text-[20px] border border-gray-500/[0.2] w-fit rounded" href={"https://github.com/abel-otegbola"}><PiGithubLogo /></a>
+          <a className="p-2 text-[20px] border border-gray-500/[0.2] w-fit rounded" href={"https://twitter.com/Abel_Otegbola"}><PiTwitterLogo /></a>
+          <a className="p-2 text-[20px] border border-gray-500/[0.2] w-fit rounded" href={"https://linkedin.com/in/abel-otegbola"}><PiLinkedinLogo/></a>
         </div>
       </div>
     </header>
