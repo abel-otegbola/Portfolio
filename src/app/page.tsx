1dfbab92044e3748972173960ca28aece953442e
ABEL OTEGBOLA <abel15655@gmail.com>
2024-10-24 09:31:37 +0100

    feat: add animation using animate-css library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ import Image from "next/image";
 import Header, { navTab } from "@/components/header/header";
 import Tab from "@/components/tab/tab";
 import Link from "next/link";
-import { motion, useViewportScroll, useTransform } from "framer-motion"
+import "animate.css/animate.compat.css"
+import ScrollAnimation from 'react-animate-on-scroll';
 
 export default function Home() {
   const navTabs: navTab[] = [
@@ -14,8 +15,6 @@ export default function Home() {
     { id: 3, label: "Contacts", to: "#contacts"},
 ]
 
-const { scrollYProgress } = useViewportScroll()
-const scale = useTransform(scrollYProgress, [2, 2], [4, 4]);
 
   return (
     <div>
@@ -23,59 +22,85 @@ const scale = useTransform(scrollYProgress, [2, 2], [4, 4]);
         <Header />
         <header className="flex md:min-h-[88vh] min-h-[50vh] items-center flex-wrap md:px-[8%] px-4 overflow-hidden">
           <div className="flex flex-col gap-4 justify-center md:items-center py-20 md:w-[75%] mx-auto">
-            <div className="relative">
-              <Image src="/images/abel.png" alt="muse" width={70} height={70} className="object-cover bottom-0 outline outline-primary/[0.2] rounded-full outline-offset-2"/>
-            </div>
-            <h1 className="md:text-center flex md:items-center gap-2 p-2 px-4 w-fit rounded-full dark:bg-black/[0.2] border border-gray-500/[0.2] text-semibold backdrop-blur-sm">
+            
+            <ScrollAnimation animateIn="zoomIn">
               <div className="relative">
-                <Image src="/images/waving.svg" alt="muse" width={24} height={24} className="object-cover bottom-0"/>
+                <Image src="/images/abel.png" alt="muse" width={70} height={70} className="object-cover bottom-0 outline outline-primary/[0.2] rounded-full outline-offset-2"/>
               </div>
-              Hi! I&apos;m Abel Otegbola
-            </h1>
-            <h2 className="md:text-[48px] text-[32px] leading-[120%] font-bold md:text-center">Transforming Ideas into Pixel Perfect Realities</h2>
-            <p className="font-medium md:text-center">I Design, Code and Build Excellent Website Projects</p>
+            </ScrollAnimation>
+            
+            <ScrollAnimation animateIn="backInDown">
+              <h1 className="md:text-center flex md:items-center gap-2 p-2 px-4 w-fit rounded-full dark:bg-black/[0.2] border border-gray-500/[0.2] text-semibold backdrop-blur-sm">
+                <div className="relative">
+                  <Image src="/images/waving.svg" alt="muse" width={24} height={24} className="object-cover bottom-0"/>
+                </div>
+                Hi! I&apos;m Abel Otegbola
+              </h1>
+            </ScrollAnimation>
+            
+            <ScrollAnimation animateIn="backInUp" duration={2}>
+              <h2 className="md:text-[48px] text-[32px] leading-[120%] font-bold md:text-center">Transforming Ideas into Pixel Perfect Realities</h2>
+            </ScrollAnimation>
+            
+            <ScrollAnimation animateIn="backInUp">
+              <p className="font-medium md:text-center">I Design, Code and Build Excellent Website Projects</p>
+            </ScrollAnimation>
             
             <Button className="font-medium rounded-full px-12 py-6">Let&apos;s chat</Button>
 
           </div>
-          <motion.div className="w-full h-full overflow-hidden rounded-[30px]"
-            style={{
-              scale
-            }}>
-            <motion.div className="overflow-hidden"
-              style={{
-                scaleY: scrollYProgress,
-                scaleX: scrollYProgress 
-              }}
-            >
+          <ScrollAnimation animateIn="zoomIn">
             <video width="100%" height="100%" controls autoPlay loop className="mx-auto md:my-12 mb-12 rounded-[10px]">
               <source src="/BOS.mp4" type="video/mp4" />
               <source src="/BOS.ogg" type="video/ogg" />
               Your browser does not support the video tag.
             </video>
-            </motion.div>
-          </motion.div>
+          </ScrollAnimation>
         </header>
       </div>
 
       <section className="md:p-[8%] md:py-[10%] p-4 py-20 bg-[#1C1C1C] text-white/[0.8] flex justify-between items-center flex-wrap gap-[60px]">
         <div className="flex flex-col gap-2 md:w-[40%] w-full">
-          <p className="font-bold md:text-[32px] text-[24px]">Every great design begins with an even greater story</p>
+          
+          <ScrollAnimation animateIn="backInDown" duration={2}>
+            <p className="font-bold md:text-[32px] text-[24px]">Every great design begins with an even greater story</p>
+          </ScrollAnimation>
+
           <div className="flex gap-6 items-center mt-6">
             <div className="flex gap-2 items-center w-[100px] uppercase text-[12px]">
-              <h2 className="text-[36px]">4</h2>
-              <p>years experience</p>
+            
+              <ScrollAnimation animateIn="backInLeft" duration={2}>
+                <h2 className="text-[36px]">4</h2>
+              </ScrollAnimation>
+              
+              <ScrollAnimation animateIn="backInUp" duration={2}>
+                <p>years experience</p>
+              </ScrollAnimation>
+              
             </div>
             <div className="flex gap-2 items-center w-[200px] uppercase text-[12px]">
-              <h2 className="text-[36px]">20</h2>
-              <p>Awesome Projects Completed</p>
+              
+              <ScrollAnimation animateIn="backInLeft" duration={2}>
+                <h2 className="text-[36px]">20</h2>
+              </ScrollAnimation>
+              
+              <ScrollAnimation animateIn="backInUp" duration={2}>
+                <p>Awesome Projects Completed</p>
+              </ScrollAnimation>
+              
             </div>
           </div>
         </div>
         
         <div className="flex flex-col gap-6 md:px-[6%] md:w-[50%]">
-          <p className="font-bold text-primary uppercase text-[18px]">About me</p>
-          <p>Since beginning my journey as a designer and developer, I&apos;ve done remote work for agencies, consulted for startups and collaborated with talented people to create digital products for both businesses and consumer use.</p>
+          <ScrollAnimation animateIn="backInDown" duration={2}>
+            <p className="font-bold text-primary uppercase text-[18px]">About me</p>
+          </ScrollAnimation>
+          
+          <ScrollAnimation animateIn="backInUp" duration={2}>
+            <p>Since beginning my journey as a designer and developer, I&apos;ve done remote work for agencies, consulted for startups and collaborated with talented people to create digital products for both businesses and consumer use.</p>
+          </ScrollAnimation>
+          
         </div>
       </section>
 
@@ -94,12 +119,25 @@ const scale = useTransform(scrollYProgress, [2, 2], [4, 4]);
               ].map(experience => (
                 <div key={experience.id} className="flex flex-wrap gap-6 items-center justify-between py-6 border-y border-gray-500/[0.3]">
                   <div className="flex flex-col gap-2 md:w-[25%] w-full">
-                    <h2 className="md:text-[20px] text-[16px] font-bold uppercase">{experience.company}</h2>
-                    <p className="font-light">{experience.date}</p>
+                    <ScrollAnimation animateIn="lightSpeedInLeft" duration={2}>
+                      <h2 className="md:text-[20px] text-[16px] font-bold uppercase">{experience.company}</h2>
+                    </ScrollAnimation>
+                      
+                    <ScrollAnimation animateIn="lightSpeedInLeft" duration={2}>
+                      <p className="font-light">{experience.date}</p>
+                    </ScrollAnimation>
+                    
                   </div>
                   <div className="flex flex-col gap-2 md:w-[30%] w-full">
-                    <h2 className="text-[16px] font-bold">{experience.position}</h2>
-                    <p className="font-light">{experience.description}</p>
+                    
+                    <ScrollAnimation animateIn="lightSpeedInLeft" duration={2}>
+                      <h2 className="text-[16px] font-bold">{experience.position}</h2>
+                    </ScrollAnimation>
+                      
+                    <ScrollAnimation animateIn="lightSpeedInLeft" duration={2}>
+                      <p className="font-light">{experience.description}</p>
+                    </ScrollAnimation>
+                    
                   </div>
                   <div className="flex items-center flex-wrap gap-2 md:w-[35%] w-full">
                     {
@@ -118,18 +156,6 @@ const scale = useTransform(scrollYProgress, [2, 2], [4, 4]);
         <p className="font-bold md:text-center text-primary uppercase text-[18px] mb-4">Portfolio</p>
         <p className="md:text-center md:text-[36px] text-[24px] font-bold">Recent Projects</p>
 
-        {/* <div className="flex gap-1 md:justify-center items-center py-6" id="projects">
-          <Button href="/contact" className="rounded-full border-gray-500/[0.3] text-gray-500" variant="secondary">
-            All
-          </Button>
-          <Button href="/contact" className="rounded-full border-gray-500/[0.3] text-gray-500" variant="secondary">
-            Design
-          </Button>
-          <Button href="/contact" className="rounded-full border-gray-500/[0.3] text-gray-500" variant="secondary">
-            Frontend
-          </Button>
-        </div> */}
-
         <div className="grid md:grid-cols-3 gap-8 mt-10">
           {
             [
@@ -139,12 +165,11 @@ const scale = useTransform(scrollYProgress, [2, 2], [4, 4]);
               { id: 1, title: "Blinks", href: "https://dribbble.com/shots/19444285-Checkout-page", img: "/images/blinks.png" },
             ].map(project => (
               <div key={project.id} className="flex flex-col justify-between">
-                <div className="relative w-full">
+                <Link href={project.href} className="relative w-full border border-gray-500/[0.2]">
                   <Image src={project.img} alt="bos" width={600} height={400} className="object-cover bg-primary/[0.2] bottom-0 rounded-[30px]"/>
-                </div>
+                </Link>
                 <div className="flex flex-col gap-2 p-3">
-                  <h2 className="font-bold text-[24px]">{project.title}</h2>
-                  <Link href={project.href}>View project</Link>
+                  <h2 className="font-bold text-[18px] uppercase">{project.title}</h2>
                 </div>
               </div>
             ))
