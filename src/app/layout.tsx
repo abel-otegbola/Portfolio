2be7263ba1442b3f2795f394500aa9ce0e1b3896
abel-otegbola <abel15655@gmail.com>
2025-06-04 13:09:02 +0100

    style: experience styles and logos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Roboto } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "../components/footer/footer";
 import Header from "@/components/header/header";
 
-const roboto = Roboto({
-  weight: ['400', '700'],
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-  display: 'swap',
-})
+const inter = Inter({ subsets: ['latin'] })
+
 export const metadata: Metadata = {
   title: "Abel Otegbola Portfolio",
   description: "Full stack website developer and UI designer with experience building amazing products.",
@@ -25,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en" className=" scroll-smooth ">
       <body
-        className={`${roboto.className} antialiased text-[14px] text-[#000] bg-[#F6FFFC] dark:bg-[#101010] dark:text-white/[0.9]`}
+        className={`${inter.className} antialiased text-[14px] text-[#000] bg-[#F6FFFC] dark:bg-[#101010] dark:text-white/[0.9]`}
       >
         <Header />
         
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,21 @@
 import Button from "@/components/button/button";
 import Image from "next/image";
 import "animate.css/animate.compat.css"
-import { ArrowUpRight, File } from "@phosphor-icons/react";
+import { ArrowUpRight, File, Phone } from "@phosphor-icons/react";
 import ContactForm from "@/components/contactForm/form";
 import { projects } from "@/data/projects";
 import Projectcard from "@/components/projectCard/projectCard";
 import AnimateText from "@/components/animations/animateText";
 import Animate from "@/components/animations/animate";
 import { useState } from "react";
+import BOSLogo from "@/assets/bos-logo";
 
 export default function Home() {
   const [selectedContact, setSelectedContact] = useState("Book a call")
 
   return (
     <div>
-      <div className="w-full bg-[url('/images/bg-2.webp')] bg-cover bg-center bg-no-repeat">
+      <div className="w-full">
         <header className="flex flex-col w-full items-center md:flex-nowrap min-h-[85vh] flex-wrap md:px-[15%] px-4 overflow-hidden">
           <div className="flex flex-col gap-4 py-20 md:w-[100%]">
             <div className="flex gap-6 items-center">
@@ -26,37 +27,43 @@ export default function Home() {
               </Animate>
             </div>
             
-            <AnimateText animateOnce={true} className="md:text-[44px] text-[32px] leading-[120%] font-black">
-              Transforming Ideas into Pixel Perfect Realities
+            <AnimateText animateOnce={true} className="md:text-[44px] text-[32px] leading-[120%] font-black text-justify">
+              Turning Ideas into Pixel-Perfect Digital Experiences
             </AnimateText>
             
-            <AnimateText animateOnce={true} speed={10} className="text-[20px]">Since beginning my journey as a designer and developer, I&apos;ve done remote work for agencies, consulted for startups and collaborated with talented people to create digital products for both businesses and consumer use.</AnimateText>
+            <AnimateText animateOnce={true} speed={10} className="text-[14px] text-justify">
+              I&apos;m Abel, a freelance designer and developer working remotely from Nigeria. I specialize in crafting clean, user-focused digital products, from websites and web apps to intuitive interfaces and brand experiences.
+            </AnimateText>
             
-            <div className="flex gap-[60px] items-center my-4">
-              <div className="flex gap-4 items-center w-[100px] uppercase text-[12px]">
+            <AnimateText animateOnce={true} speed={10} className="text-[14px] text-justify">
+              Over the years, I&apos;ve had the privilege of collaborating with startups, remote agencies, and creative teams to bring ideas to life online. Whether it&apos;s refining a marketing site or designing a product UI from the ground up, I&apos;m passionate about creating designs that are beautiful and functional.
+            </AnimateText>
+            
+            <div className="flex gap-[70px] items-center my-4">
+              <div className="flex gap-2 items-center w-[100px] uppercase text-[12px]">
                 <AnimateText animateOnce={true} className="text-[20px] font-bold p-4 py-2 rounded-full border border-gray-500/[0.2]">
                   4
                 </AnimateText>
                 
                 <AnimateText animateOnce={true}>years experience</AnimateText>
               </div>
-              <div className="flex gap-4 items-center w-[200px] uppercase text-[12px]">
+              <div className="flex gap-2 items-center w-[220px] uppercase text-[12px]">
                 <AnimateText animateOnce={true} className="text-[20px] font-bold p-3 py-2 rounded-full border border-gray-500/[0.2]">20</AnimateText>
                 
                 <AnimateText animateOnce={true}>Awesome Projects Completed</AnimateText>
               </div>
             </div>
             
-            <div className="flex gap-2 items-center flex-wrap">
+            <div className="flex gap-2 flex-wrap">
               <Animate>
-                <Button href="#contacts" className="font-medium pl-1 py-[4px] pr-5 rounded-[30px]">
-                  <Image src="/images/abel.png" alt="muse" width={30} height={30} className="object-cover bottom-0 rounded-full"/>
+                <Button href="#contacts" className="font-medium pl-1 py-[4px] pr-5 rounded-[30px] dark:bg-gray-500/[0.5]">
+                  <span className="rounded-full bg-white w-[30px] h-[30px] flex items-center justify-center text-black"><Phone weight="bold" size={16} /></span>
                   Book a call or send a message
                 </Button>
               </Animate>
               <Animate>
                 <Button href="https://drive.google.com/file/d/1A8Z-h_qobLQESzUxzyoIR6ouuey_GyFM/view?usp=sharing" className="font-medium pl-1 py-[4px] pr-5 rounded-[30px] bg-primary">
-                  <span className="rounded-full bg-white w-[27px] h-[27px] flex items-center justify-center text-black"><File weight="bold" size={16} /></span>
+                  <span className="rounded-full bg-white w-[30px] h-[30px] flex items-center justify-center text-black"><File weight="bold" size={16} /></span>
                   Resume
                 </Button>
               </Animate>
@@ -67,18 +74,22 @@ export default function Home() {
       </div>
 
       
-      <section className="md:px-[15%] py-[60px] p-4 bg-[#000] bg-[url('/images/bg-2.webp')] bg-cover bg-center bg-no-repeat text-white">
-          <AnimateText animateOnce={true} className="font-bold uppercase md:text-[28px] text-[18px] mb-4">My Experience</AnimateText>
-          <AnimateText animateOnce={true} className="md:text-[16px] text-[14px] font-medium">Explore My Design and Development Journey</AnimateText>
+      <section className="md:px-[15%] py-[60px] p-4 bg-white dark:bg-[#000]">
+          <p className="font-bold uppercase text-[18px] mb-4">My Experience</p>
+          <p className="font-medium">Explore My Design and Development Journey</p>
 
-          <div className="grid md:grid-cols-2 grid-cols-1 gap-6 mt-[50px]">
-            <div className=" flex flex-col gap-6 md:p-8 p-4 md:rounded-[10px] rounded break-inside-avoid bg-gradient-to-br from-primary to-[#000]/[0.6] border border-[#000]/[0.6]">
-              <div className="flex justify-between gap-6">
-                <AnimateText animateOnce={true} className="text-sm opacity-[0.7]">Fortbridge</AnimateText>
-                <AnimateText animateOnce={true}>June 2024 - Present</AnimateText>
-              </div>
-              <AnimateText animateOnce={true} className="text-2xl font-bold">HUBSTACK</AnimateText>
-              <AnimateText animateOnce={true} className="mb-8">A fintech application to send money, pay bills and recharge your account </AnimateText>
+          <div className="grid md:grid-cols-2 grid-cols-1 md:gap-12 gap-6 mt-[50px]">
+            <div className="flex flex-col gap-3 md:py-8 py-4 break-inside-avoid border-y border-gray-500/[0.2]">
+              <Image
+                src={"/hubstack.png"}
+                alt={"hubstack"}
+                width={30}
+                height={30}
+              />
+              <p className="text-md font-bold">Frontend Developer, Fortbridge</p>
+              <p className="">At FortBridge, my role was to design and develop responsive, user-centric interfaces that translated complex functionality into seamless digital experiences. I focused on crafting clean, maintainable frontends that aligned with both product goals and brand standards, ensuring consistency and clarity across all user touchpoints.</p>
+              
+              <p className="text-[12px] mb-4">June 2024 - Present</p>
 
               <div className="grid grid-cols-3 gap-4">
                 
@@ -97,21 +108,17 @@ export default function Home() {
             </div>
 
             <div className="flex flex-col gap-6">
-              <div className="h-full flex flex-col gap-6 md:p-8 p-4 md:rounded-[10px] rounded break-inside-avoid border border-gray-500/[0.2]">
-                <div className="flex justify-between gap-6">
-                  <AnimateText animateOnce={true} className="text-sm opacity-[0.7]">Bos</AnimateText>
-                  <AnimateText animateOnce={true}>January 2024 - June 2024</AnimateText>
-                </div>
-                <AnimateText animateOnce={true} className="text-2xl font-bold">BOS UNLIMITED</AnimateText>
-                <AnimateText animateOnce={true}>An ecommerce website for sale of customizable items and clothings</AnimateText>
-              </div>
-              <div className="h-full flex flex-col gap-6 md:p-8 p-4 md:rounded-[10px] rounded break-inside-avoid bg-gradient-to-br from-transparent to-[#000]/[0.4] border border-gray-500/[0.2]">
-                <div className="flex justify-between gap-6">
-                  <AnimateText animateOnce={true} className="text-sm opacity-[0.7]">Xatalyst</AnimateText>
-                  <AnimateText animateOnce={true}>September 2023 - November 2023</AnimateText>
-                </div>
-                <AnimateText animateOnce={true} className="text-2xl font-bold">XATALYST FITNESS APP</AnimateText>
-                <AnimateText animateOnce={true}>Fitness startup, Website development, Responsive</AnimateText>
+            <div className="flex flex-col gap-3 md:py-8 py-4 break-inside-avoid border-y border-gray-500/[0.2]">
+                <BOSLogo width={40} height={40} />
+                <p className="text-md font-bold">Full-stack Developer, BOS Unlimited</p>
+                <p>I contributed to the development and maintenance of a scalable, high-performing e-commerce platform as a fullstack developer. Working across both frontend and backend systems, I helped bring fashion-focused digital experiences to life.</p>
+                <p className="text-[10px]">January 2024 - June 2024</p>
+                <Image
+                    src={'/images/bos.webp'}
+                    alt={'bos'}
+                    width={1000}
+                    height={373}
+                  />
               </div>
             </div>
 
@@ -119,8 +126,8 @@ export default function Home() {
       </section>
 
       <section className="md:p-[8%] py-[60px] p-4 pb-10 flex flex-col md:items-center gap-4">
-        <AnimateText animateOnce={true} className="font-bold md:text-center md:text-[28px] text-[18px]">Optimization, Redesign and Conversion</AnimateText>
-        <AnimateText animateOnce={true} className="md:text-center md:text-[16px] text-[14px] font-medium">He had in-depth knowledge in what he was doing. I loved how he finished it fast with accuracy</AnimateText>
+        <p className="font-bold md:text-center md:text-[28px] text-[18px]">Optimization, Redesign and Conversion</p>
+        <p className="md:text-center font-medium">He had in-depth knowledge in what he was doing. I loved how he finished it fast with accuracy</p>
         
         <Animate>
           <Button href="#contacts" className="font-medium pl-1 py-[4px] pr-8 rounded-[30px] text-center">
@@ -133,7 +140,7 @@ export default function Home() {
 
       <section className="md:px-[15%] py-[60px] p-4 pb-10">
 
-        <AnimateText animateOnce={true} className="font-bold md:text-center uppercase md:text-[28px] text-[18px]">Recent Projects</AnimateText>
+        <AnimateText animateOnce={true} className="font-bold md:text-center uppercase text-[18px]">Recent Projects</AnimateText>
         <div className="columns-1 items-stretch gap-4 mt-10">
           {
             projects.slice(0,12)?.map(project => (
@@ -145,8 +152,8 @@ export default function Home() {
 
       <section className="flex flex-col gap-6 md:px-[15%] py-[60px] p-4 bg-cover bg-center bg-no-repeat bg-gray-100/[0.2] dark:bg-[#EEE3E3]/[0.06]">
         <div className="flex flex-col items-center justify-center gap-4">
-          <h1 className="font-bold md:text-center uppercase md:text-[28px] text-[18px]">Contact me</h1>
-          <p className="md:text-[16px] text-[14px] font-medium">Are you ready to turn your idea into a reality?</p>
+          <h1 className="font-bold md:text-center uppercase text-[18px]">Contact me</h1>
+          <p className="font-medium md:text-center">I’m keen to learn more about what your company does, and specific details about the project. The more information you can provide, the better!</p>
           <div className="flex gap-1 items-center border border-black p-1 rounded-full">
           {
             ["Book a call", "Send a message"].map(contact => (
